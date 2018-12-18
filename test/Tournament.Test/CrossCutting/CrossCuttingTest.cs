9f4cdb309c887f20cec90454d4fab8ff12a35530
Marcio Adão <marc.adans@gmail.com>
2018-12-18 00:55:45 -0200

    launch

---
+++ test/Tournament.Test/CrossCutting/CrossCuttingTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tournament.CrossCutting;
+using Tournament.Domain.Lambda3Context.Models;
+using Xunit;
+
+namespace Tournament.Test.CrossCutting
+{
+    public class CrossCuttingTest
+    {
+        [Fact]
+        public async Task Should_GetHttpClientAsync()
+        {
+            var result = await HttpClientService
+                .GetAsync<List<Lambda3Movie>>("https://copadosfilmes.azurewebsites.net/api/filmes")
+                .ConfigureAwait(false);
+
+            Assert.NotNull(result);
+            Assert.True(result.Any());
+        }
+    }
+}
---
+++ test/Tournament.Test/Helpers/RequestProfileTest.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Tournament.Domain.Lambda3Context.Models;
+using Tournament.Domain.MovieContext.Models;
+
+namespace Tournament.Test
+{
+    public class RequestProfileTest : Profile
+    {
+        public RequestProfileTest()
+        {
+            CreateMap<Lambda3Movie, Movie>();
+        }
+    }
+}
